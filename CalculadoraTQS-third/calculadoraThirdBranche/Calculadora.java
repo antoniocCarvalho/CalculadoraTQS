ba62c654808ed7d0e315ad0dbb95c4c590b4fab1
Antonio Celso Carvalho <110407383+antoniocCarvalho@users.noreply.github.com>
2023-10-29 19:04:13 -0300

    Add files via upload

---
+++ CalculadoraTQS-third/calculadoraThirdBranche/Calculadora.java
@@ -0,0 +1,44 @@
+package calculadora;
+/**
+* @author antoniocarvalho
+* @version 1.05
+* @since release 1.02 da aplicacão
+* 
+* 
+*  Uma classe simples que realiza todas as operacões
+*/
+public class Calculadora {
+/**
+ * 
+ * @param a O primeiro operando
+ * @param b o segundo operando
+ * @return O resultado da operação
+ */
+	
+	int somar(int a, int b){
+        return a + b;
+    }
+    
+    
+    public int sub(int a, int b){
+        return a - b;
+    }
+   
+    
+    public int multi(int a, int b){
+        return a * b;
+    }
+    
+    
+    public int div(int a, int b){
+        return a / b;
+    }
+}
+
+
+
+
+
+
+
+
